39e6a4a83e5ad4fea8c82ef36f388e8fd65dcd4e
Nathan Garcia <nathangar01@gmail.com>
2021-06-07 18:38:32 -0300

    Create Users Repository In Memory

---
+++ src/modules/accounts/repositories/implementations/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,39 @@
+import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
+import { User } from "../../../entities/User";
+import { IUsersRepository } from "../../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+    users: User[] = [];
+
+    async create({
+        name,
+        email,
+        password,
+        driver_license,
+    }: ICreateUserDTO): Promise<void> {
+        const user = new User();
+
+        Object.assign(user, {
+            name,
+            email,
+            password,
+            driver_license,
+        });
+
+        this.users.push(user);
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        return this.users.find((user) => user.email === email);
+    }
+
+    async findByID(id: string): Promise<User> {
+        return this.users.find((user) => user.id === id);
+    }
+
+    async list(): Promise<User[]> {
+        return this.users;
+    }
+}
+
+export { UsersRepositoryInMemory };
