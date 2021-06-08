2a10ae04bb9b7a9929168722c982177cf88c1c32
Nathan Garcia <nathangar01@gmail.com>
2021-06-08 15:31:49 -0300

    Imports refactoring

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { CreateSpecificationController } from "../modules/cars/useCases/createSpecifications/CreateSpecificationController";
-import { ListSpecificationController } from "../modules/cars/useCases/listSpecifications/ListSpecificationController";
+import { ensureAuthenticated } from "@middlewares/ensureAuthenticated";
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecifications/CreateSpecificationController";
+import { ListSpecificationController } from "@modules/cars/useCases/listSpecifications/ListSpecificationController";
 
 const specificationsRoutes = Router();
 
